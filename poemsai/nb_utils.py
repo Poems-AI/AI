0baccc734c94c07f5da55c76308743e735dd599e
davidleonfdez <dleoferu@gmail.com>
2022-02-03 02:55:39 +0000

    Test fix of nb utils

--- poemsai/nb_utils.py
+++ poemsai/nb_utils.py
@@ -5,21 +5,22 @@ __all__ = ['download_checkpoint_from_hf_hub', 'commit_checkpoint_to_hf_hub']
 
 
 def download_checkpoint_from_hf_hub(model_name, user, pwd):
-    !git clone {model_to_url(custom_model_name, user, pwd)}
+    model_url = model_to_url(custom_model_name, user, pwd)
+    get_ipython().system("git clone {model_url}")
 
 
-def commit_checkpoint_to_hf_hub(model_name, user, checkpoint_path, message='Update model',
-                                base_repo_path='./', pwd=None):
-    cwd = os.getcwd()
-    if base_repo_path[-1] != '/': base_repo_path += '/'
-    repo_path = Path(base_repo_path + model_name)
-    if not repo_path.exists():
-        repo_path.mkdir()
-        !git clone {model_to_url(model_name, user, pwd=pwd)} $repo_path
-    !cp {checkpoint_path}/* $repo_path
-    %cd $repo_path
-    !git add .
-    !git status
-    !git commit -m "{message}"
-    !git push
-    %cd $cwd
+# def commit_checkpoint_to_hf_hub(model_name, user, checkpoint_path, message='Update model',
+#                                 base_repo_path='./', pwd=None):
+#     cwd = os.getcwd()
+#     if base_repo_path[-1] != '/': base_repo_path += '/'
+#     repo_path = Path(base_repo_path + model_name)
+#     if not repo_path.exists():
+#         repo_path.mkdir()
+#         !git clone {model_to_url(model_name, user, pwd=pwd)} $repo_path
+#     !cp {checkpoint_path}/* $repo_path
+#     %cd $repo_path
+#     !git add .
+#     !git status
+#     !git commit -m "{message}"
+#     !git push
+#     %cd $cwd
