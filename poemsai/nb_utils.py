ebb485b9d962a95ea1af219f4d13223ab7579e9a
davidleonfdez <dleoferu@gmail.com>
2022-02-03 03:10:23 +0000

    Fix nb utils

--- poemsai/nb_utils.py
+++ poemsai/nb_utils.py
@@ -10,18 +10,20 @@ def download_checkpoint_from_hf_hub(model_name, user, pwd):
     get_ipython().system("git clone {model_url}")
 
 
-# def commit_checkpoint_to_hf_hub(model_name, user, checkpoint_path, message='Update model',
-#                                 base_repo_path='./', pwd=None):
-#     cwd = os.getcwd()
-#     if base_repo_path[-1] != '/': base_repo_path += '/'
-#     repo_path = Path(base_repo_path + model_name)
-#     if not repo_path.exists():
-#         repo_path.mkdir()
-#         !git clone {model_to_url(model_name, user, pwd=pwd)} $repo_path
-#     !cp {checkpoint_path}/* $repo_path
-#     %cd $repo_path
-#     !git add .
-#     !git status
-#     !git commit -m "{message}"
-#     !git push
-#     %cd $cwd
+def commit_checkpoint_to_hf_hub(model_name, user, checkpoint_path, message='Update model',
+                                base_repo_path='./', pwd=None):
+    shell = get_ipython()
+    cwd = os.getcwd()
+    if base_repo_path[-1] != '/': base_repo_path += '/'
+    repo_path = Path(base_repo_path + model_name)
+    if not repo_path.exists():
+        repo_path.mkdir()
+        model_url = model_to_url(model_name, user, pwd=pwd)
+        shell.system("!git clone {model_url} $repo_path")
+    shell.system("cp {checkpoint_path}/* $repo_path")
+    shell.run_line_magic('cd', str(repo_path))
+    shell.system("git add .")
+    shell.system("git status")
+    shell.system('git commit -m "{message}"')
+    shell.system("git push")
+    shell.run_line_magic('cd', cwd)
